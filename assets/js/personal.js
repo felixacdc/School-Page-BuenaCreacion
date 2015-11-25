423b205dec9adeb6979c33525a94c98187e28473
Felix Mendez <pfmata01@gmail.com>
2015-11-25 17:15:22 -0600

    [modification] historical review and contacts, added information and textarea

--- assets/js/personal.js
+++ assets/js/personal.js
@@ -1,13 +1,29 @@
+var url;
+
+function clean(){
+	$("textarea").val(" ");
+	$("textarea").val($("textarea").val().trim());
+
+	$("input").val(" ");
+	$("input").val($("textarea").val().trim());
+}
+
 $(document).ready(function(){
+
+	clean();
 
 	$("#first").addClass("activeOption");
 	$("#first a").click();
 
-	
-	
 	$(".navbar-nav li").click(function(){
 		$(".activeOption").removeClass("activeOption");
 		$(this).addClass("activeOption");
 	});	
+
+	$("#face").click(function(){
+		url = "https://www.facebook.com/Colegio-Buena-Creaci%C3%B3n-250623778420782/";
+		var win = window.open(url, '_blank');
+  		win.focus();
+	});
 
 });
