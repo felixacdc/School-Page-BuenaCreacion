c58ee6f081d25355393cef1ae07973cbeb936116
Felix Mendez <pfmata01@gmail.com>
2015-11-26 22:28:56 -0600

    [modification] function send email

--- Models/class.SendEmail.php
+++ Models/class.SendEmail.php
@@ -7,23 +7,46 @@ class SendEmail
 {
 	private $email;
 	private $message;
-	private $title;
+	public static $title;
+	public static $headers;
+	public static $addressee;
+	public static $bodyHtml;
 
 	function __construct($email, $message)
 	{
 		$this->email = $email;
 		$this->message = $message;
-		$this->title = 'Mensaje Contacto de colegiobuenacreacion.reu.gt';
+		
 	}
 
-	public function fnSendEmail(){
+	public static function fnSendMessage($valueEmal, $valueMessage)
+	{
+		self::$title = 'Mensaje de Contacto';
 
-		if (mail($this->email, $this->title, $this->message)) {
-			return 'si se envio';
+		self::$headers = 'From: ' . $valueEmal . "\r\n" .
+	    'Content-Type: text/html; charset=iso-8859-1' . "\r\n" .
+	    'X-Mailer: PHP/' . phpversion();
+
+	    self::$addressee = '94felix23pablo2@gmail.com';
+
+	    self::$bodyHtml = '<div style="background-color:#4B71AB; padding:10px;">
+	    					<h2 style="color:#FFFFFF; font-size:30px; font-weight:100; line-height:115%; text-align:center;">
+	    					Mensaje del contacto</h2></div>
+	    					<div style="background-color:#F0F0F0; padding:40px;">
+	    					<p>' .$valueMessage . '</p></div>';
+	}
+
+	public function fnSendEmail()
+	{
+
+		$this::fnSendMessage($this->email, $this->message);
+
+		if (mail(self::$addressee, self::$title, self::$bodyHtml, self::$headers)) {
+			header('location: ../index.html');		
 		} else {
-			return 'no se envio';
+			header('location: ../index.html');
 		}
-		
+
 	}
 
 }
